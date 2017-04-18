75ca5de4e0bc8f9e581c9fffd34f74c8e4101e1e
Garrett Credi <gcc@ameritech.net>
2017-04-18 20:55:53 +0000

    Adding features for freeCodeCamp project. Adding ip, userAgent, and language detection

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 var express = require('express');
+var app = express();
+
+app.use('/api/whoami', function(req, res){
+    var ip = req.headers['x-forwarded-for'];
+    var userAgent = req.headers['user-agent'];
+    var lang = req.headers['accept-language'].split(',')[0];
+    var data = {
+        'ip-address':ip,
+        'language':lang,
+        'software':userAgent
+    }
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    res.end(JSON.stringify(data));
+});
+app.listen(8080);
